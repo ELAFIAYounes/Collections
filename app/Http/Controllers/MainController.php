3e5fc2984a492b80e551cf05dce55cbbaf4d7e12
EL AFIA Younes <elafia847@gmail.com>
2022-01-29 17:16:16 +0100

    add array of object and use lambda expression

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 class MainController extends Controller
 {
-    public $integerData = [21,32,43,50];
+    protected $integersData = [21,32,43,50];
+    protected $objectsData = [
+        ['column_1' => 30000,'column_2' => 400],
+        ['column_1' => 20000,'column_2' => 400],
+        ['column_1' => 10000,'column_2' => 400]
+    ];
 
-    public function average(){
-        return  collect($this->integerData)->average();
+    public function integersAverage(){
+        return  collect($this->integersData)->average();
+    }
+
+    public function objectsAverage(){
+        return collect($this->objectsData)->average(function($value){
+            return $value['column_1'] + $value['column_2'];
+        });
     }
 }
